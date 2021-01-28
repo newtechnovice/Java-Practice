a811729bf2660f1ec24cf34c65cf10f3e2b9f336
newtechnovice <66836831+newtechnovice@users.noreply.github.com>
2021-01-28 11:29:23 +0800

    added cylinder subclass

--- OOP/Circle/Circle.java
+++ OOP/Circle/Circle.java
@@ -3,22 +3,82 @@ public class Circle {
     private double radius;
     private String color;
 
-    public Circle (){
+    public Circle(){
         radius = 1.0;
         color = "red";
     }
 
+    public Circle(double radius){
+        this.radius = radius;
+        color = "red";
+    }
 
-public Circle(double r){
-    radius = r;
-    color = "red";
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+    public String toString(){
+        return "Cirlce[radius = "+ radius + " color = "+ color +"]";
+    }
 }
 
-public double getRadius(){
-    return radius;
-}
+class Cylinder extends Circle{
+    private double height;
 
-public double getArea(){
-    return radius*radius*Math.PI;
-}
+    public Cylinder(){
+        super();
+        height = 1.0;
+    }
+
+    public Cylinder(double radius){
+        super(radius);
+    }
+
+    public Cylinder(double radius, double height){
+        super(radius);
+        this.height = height;
+    }
+
+    public Cylinder(double radius, String color, double height){
+        super(radius, color);
+        this.height = height;
+    }    
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    public double getVolume(){
+        return height*getArea();
+    }
+
+    @Override
+    public String toString(){
+        return "Cylinder[height = "+ height +"]";
+    }
 }
